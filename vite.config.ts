da5c6d6a23691c14c3da98d5fa11190c5aa51c51
Roman Popov <popov.r.k@yandex.ru>
2025-01-31 15:06:46 +0500

    Add linters, some libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const pathSrc = resolve(__dirname, 'src');
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wedding-invitation/'
-})
+  base: '/wedding-invitation/',
+  resolve: {
+    alias: {
+      '@': pathSrc,
+    },
+  },
+});
