7dfb01ca7ef3833201cb58a93a0f2195eb90aeed
Roman Popov <popov.r.k@yandex.ru>
2025-02-03 15:39:57 +0500

    Added images

--- src/modules/timing/timing.tsx
+++ src/modules/timing/timing.tsx
@@ -1,8 +1,14 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable import/extensions */
 import * as React from 'react';
 import cn from 'classnames';
 import { createBlock } from '@/utils';
 import { HeadingBlock } from './heading-block';
 import { AddressBlock } from './address-block';
+import birds0 from '@/assets/images/birds/birds0.jpg';
+import birds1 from '@/assets/images/birds/birds1.jpg';
+import birds2 from '@/assets/images/birds/birds2.webp';
+import birds3 from '@/assets/images/birds/birds3.jpg';
 import './timing.scss';
 
 const block = createBlock('timing');
@@ -41,6 +47,13 @@ const Timing = () => (
           <span className="number">75</span>
         </AddressBlock>
       </div>
+    </div>
+    <div className={block.element('images-wrapper')}>
+      <img src={birds0} />
+      <img src={birds1} />
+      <img src={birds2} />
+      <img src={birds3} />
+
     </div>
   </div>
 );
