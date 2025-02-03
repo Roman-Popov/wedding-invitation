e9ad3b0e11b65e3001a8737493dc04e62a98e246
Roman Popov <popov.r.k@yandex.ru>
2025-02-03 15:13:57 +0500

    Added music

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import cn from 'classnames';
 import { createBlock, getNames } from '@/utils';
 import { Timer } from '@/components';
+// eslint-disable-next-line import/extensions
+import songUrl from '@/assets/music/intro.mp3';
 import './intro.scss';
 
 const block = createBlock('intro');
 
 const Intro = ({ isOpened }: { isOpened: boolean }) => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [shouldPlay, setShouldPlay] = useState<null | boolean>(null);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,10 +28,31 @@ const Intro = ({ isOpened }: { isOpened: boolean }) => {
     };
   }, []);
 
+  useEffect(() => {
+    if (audioRef.current && shouldPlay !== null) {
+      if (shouldPlay) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    }
+  }, [shouldPlay]);
+
   const { isSingle, noNames } = getNames();
 
   return (
     <div className={cn(block.block(), block.modifyBlock({ opened: isOpened }))}>
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      <audio ref={audioRef} src={songUrl} />
+      <button
+        className={cn(
+          block.element('toggle-song'),
+          block.modifyElement('toggle-song', { pause: Boolean(shouldPlay) }),
+        )}
+        type="button"
+        aria-label="toggle song"
+        onClick={() => setShouldPlay((prevShouldPlay) => !prevShouldPlay)}
+      />
       <div className={block.element('content')}>
         <div className={block.element('names')}>
           <span className={block.element('name')}>Роман</span>
